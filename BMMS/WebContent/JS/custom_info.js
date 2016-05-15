79a3f9262503ff533b22b2cad359e10c8de5e26c
stefenson <wkqcom@live.com>
2016-05-16 02:37:02 +0800

    已知界面加入新的刷新动画

--- BMMS/WebContent/JS/custom_info.js
+++ BMMS/WebContent/JS/custom_info.js
@@ -1,12 +1,19 @@
-var myxmlhttp = "";
+var process_time_last = 0;
+var pi_pos = new Array();
+var center_y = 50;
+var center_x = 50;
+var s_process_timer = 0;
 
-function dispose()
-{
-	if (myxmlhttp.readyState==4 && myxmlhttp.status==200)
-	{
-		location.reload();
-	}
-}
+for(var i = 0; i < 5; i++)
+	pi_pos[i] = 0;
+
+// function dispose()
+// {
+// 	if (myxmlhttp.readyState==4 && myxmlhttp.status==200)
+// 	{
+// 		location.reload();
+// 	}
+// }
 
 function confirm_click()
 {
@@ -16,17 +23,20 @@ function confirm_click()
 		return;
 	}
 
-	myxmlhttp = getXmlHttpObject();
+	process_message.style.visibility = "visible";
+	s_process_timer = setInterval("process_anime()", 10);
+
+	parent.myxmlhttp = getXmlHttpObject();
 	
-	if (myxmlhttp)
+	if (parent.myxmlhttp)
 	{
 		var aim_url = "/BMMS/AddCustomerInfo?time=" + new Date();
 		var data = "input_add_name=" + document.getElementById("input_add_name").value + "&input_add_tel=" + document.getElementById("input_add_tel").value + "&input_add_fix=" + document.getElementById("input_add_fix").value + "&input_add_phone1=" + document.getElementById("input_add_phone1").value + "&input_add_phone2=" + document.getElementById("input_add_phone2").value;
 		
-		myxmlhttp.open("post", aim_url, true);
-		myxmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-		myxmlhttp.onreadystatechange = dispose;
-		myxmlhttp.send(data);
+		parent.myxmlhttp.open("post", aim_url, true);
+		parent.myxmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		parent.myxmlhttp.onreadystatechange = parent.refresh_now_page;
+		parent.myxmlhttp.send(data);
 	}
 	// document.forms["form_post"].submit();
 }
@@ -44,17 +54,20 @@ function cancle_click()
 
 function del_click(key)
 {
-	myxmlhttp = getXmlHttpObject();
+	process_message.style.visibility = "visible";
+	s_process_timer = setInterval("process_anime()", 10);
+
+	parent.myxmlhttp = getXmlHttpObject();
 	
-	if (myxmlhttp)
+	if (parent.myxmlhttp)
 	{
 		var aim_url = "/BMMS/DelCustomerInfo?time=" + new Date();
 		var data = "del_customer_name=" + key;
 		
-		myxmlhttp.open("post", aim_url, true);
-		myxmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-		myxmlhttp.onreadystatechange = dispose;
-		myxmlhttp.send(data);
+		parent.myxmlhttp.open("post", aim_url, true);
+		parent.myxmlhttp.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		parent.myxmlhttp.onreadystatechange = parent.refresh_now_page;
+		parent.myxmlhttp.send(data);
 	}
 
 	// var postForm = document.createElement("form");
@@ -71,4 +84,30 @@ function del_click(key)
 	// document.body.appendChild(postForm);
 	// postForm.submit();
 	// document.body.removeChild(postForm);
+}
+
+function process_anime()
+{
+	process_time_last = process_time_last + 1;
+	var need_change = parseInt(process_time_last / 10);
+	if(need_change > 5)
+		need_change = 5;
+	for(var i = 0; i < need_change; i++)
+	{
+		var e = document.getElementById("pi" + i);
+		if (pi_pos[i] > 360) 
+			pi_pos[i] = pi_pos[i] - 360;
+
+		if(pi_pos[i] < 180)
+			pi_pos[i] = pi_pos[i] + (pi_pos[i] + 10) / 20;
+		else if(pi_pos[i] < 360)
+			pi_pos[i] = pi_pos[i] + (360 - pi_pos[i] + 10) / 20;
+
+		var new_x = center_x + Math.sin(2 * Math.PI / 360 * pi_pos[i]) * 50;
+		var new_y = center_y - Math.cos(2 * Math.PI / 360 * pi_pos[i]) * 50;
+
+		e.style.top = new_y + "px";
+		e.style.left = new_x + "px";
+		console.info("item info processtimer running.");
+	}
 }
